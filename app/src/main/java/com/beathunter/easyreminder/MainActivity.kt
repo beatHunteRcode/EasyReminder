999ee784ed67f885b39da8de2a35283a94393dfe
Nikita <nikemailforservises@gmail.com>
2020-09-21 20:07:43 +0300

    continue working...

--- app/src/main/java/com/beathunter/easyreminder/MainActivity.kt
+++ app/src/main/java/com/beathunter/easyreminder/MainActivity.kt
@@ -1,21 +1,23 @@
 package com.beathunter.easyreminder
 
+import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.DatePicker
 import android.widget.TimePicker
-import java.text.ParseException
-import java.text.SimpleDateFormat
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.adding_reminder.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private val mainScreen = R.layout.activity_main;
     private val addingReminderScreen = R.layout.adding_reminder;
+    private val myRemindersScreen =  R.layout.my_reminders;
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,16 +29,30 @@ class MainActivity : AppCompatActivity() {
 
     fun onAddRemButtonClick(v: View) = setContentView(addingReminderScreen)
 
-    fun onMyRemsButtonClick(v: View) {
-
-    }
+    fun onMyRemsButtonClick(v: View) = setContentView(myRemindersScreen)
 
     var timeHour: Int = 0
     var timeMinute: Int = 0
 
+
+    fun onSelectDateButtonClick(v: View) {
+        val selectDateButton : Button = findViewById(R.id.set_date_button)
+        val calendar = Calendar.getInstance();
+        val year = calendar.get(Calendar.YEAR);
+        val month = calendar.get(Calendar.MONTH);
+        val day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        set_date_button.setOnClickListener {
+            val datPickerDialog = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, mYear, mMonth , mDay ->
+                set_date_button.setText("" + mDay + "/" + mMonth + "/" + mYear);
+            }, year, month, day)
+            datPickerDialog.show()
+        }
+    }
+
     fun onSelectTimeButtonClick(v: View) {
-        var selectTimeButton : Button = findViewById(R.id.set_time_button)
-        var timePickerDialog = TimePickerDialog (
+        val selectTimeButton : Button = findViewById(R.id.set_time_button)
+        val timePickerDialog = TimePickerDialog (
             this,
             android.R.style.Theme_Holo_Light_Dialog_MinWidth,
             object : TimePickerDialog.OnTimeSetListener {
@@ -52,6 +68,10 @@ class MainActivity : AppCompatActivity() {
         timePickerDialog.getWindow()?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         timePickerDialog.updateTime(timeHour, timeMinute)
         timePickerDialog.show()
+    }
+
+    fun onCreateButtonPressed(v : View) {
+
     }
 
     override fun onBackPressed() = setContentView(mainScreen)
