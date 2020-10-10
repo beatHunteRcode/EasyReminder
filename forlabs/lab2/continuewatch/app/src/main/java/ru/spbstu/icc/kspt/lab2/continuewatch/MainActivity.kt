d1c9556f40656d1dd9ce0b1a95b4dc490330d992
beatHunteR <nikitasherepa@gmail.com>
2020-10-10 09:31:21 +0300

    corrected mistakes in continuewatch

---
+++ forlabs/lab2/continuewatch/app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
@@ -0,0 +1,71 @@
+package ru.spbstu.icc.kspt.lab2.continuewatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private val TAG : String = "lifecycle"
+    var secondsElapsed: Int = 0
+    var isRunning = false
+
+    var backgroundThread = Thread {
+        while (true) {
+            Thread.sleep(1000)
+            if (isRunning) {
+                textSecondsElapsed.post {
+                    textSecondsElapsed.setText("Seconds elapsed: " + secondsElapsed++)
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        backgroundThread.start()
+        Log.d(TAG, "Activity onCreate(): created")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "Activity onStart(): started")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        isRunning = true
+        Log.d(TAG, "Activity onResume(): resumed")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        isRunning = false
+        Log.d(TAG, "Activity onPause(): paused")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "Activity onStop(): stopped")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "Activity onDestroy(): destroyed")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("seconds", secondsElapsed)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        secondsElapsed = savedInstanceState.getInt("seconds")
+        textSecondsElapsed.post {
+            textSecondsElapsed.setText("Seconds elapsed: " + secondsElapsed)
+        }
+    }
+}
